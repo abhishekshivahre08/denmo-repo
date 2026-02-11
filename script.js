32c605e6d1f0739c07eb1927e53d41f4da0d8b2d
Abhishek Shivhare <abhishekshivahre08@gmail.com>
2026-02-11 23:08:37 +0530

    demo repo

---
+++ script.js
@@ -0,0 +1,14 @@
+class Animal {
+    constructor() {
+        console.log("object are created......")
+    }
+    eats() {
+        console.log("kha raha hu")
+    }
+    jump() {
+        console.log("nach raha hu")
+    }
+
+}
+let a = new Animal()
+console.log(a)
